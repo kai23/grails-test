814a83e739dce5e7ad9bbf2d32c92766645a8abf
Florian CHEVALLIER <kai_23_@hotmail.Com>
2013-10-20 17:14:16 +0200

    bootstrap ends now

--- grails-app/conf/ApplicationResources.groovy
+++ grails-app/conf/ApplicationResources.groovy
@@ -2,4 +2,13 @@ modules = {
     application {
         resource url:'js/application.js'
     }
+   bootstrap {
+        resource url:'less/custom-bootstrap.less',attrs:[rel: "stylesheet/less", type:'css']
+        dependsOn 'jquery'
+        dependsOn 'bootstrapJs'
+    }
+
+    bootstrapJs {
+        resource url:'js/bootstrap/bootstrap.min.js'
+    }
 }
